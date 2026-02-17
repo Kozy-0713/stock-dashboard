6a6181394872792a6236b0486ab04383d2ede073
Kozy-0713 <kishino.yumemi@gmail.com>
2026-02-17 23:51:54 +0900

    Fix currency display and implement layout components

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,7 @@
+<nav class="main-nav">
+    <div class="logo">Stock Manager</div>
+    <ul>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/stocks">Inventory</a></li>
+    </ul>
+</nav>
