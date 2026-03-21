050166b421c9d36393a20e8a4157de29f7ffcfa3
Kozy-0713 <kishino.yumemi@gmail.com>
2026-03-21 22:27:12 +0900

    https強制

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        // 本番環境（Render）では、生成するURLをすべてHTTPSに強制する
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
